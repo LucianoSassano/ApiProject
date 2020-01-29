66e282ba7d02e9bae79693b87653b3a0ec7fdc2a
Martin Gonzalez <martin.hgonzalez21@gmail.com>
2020-01-29 00:44:02 -0300

    Simplified to only 1 function to fetch from api

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-var random10Jokes;
 var randomJoke;
 
-function fetch10FromApi() {
-  async function getJokesApi() {
-    try {
-      config = {
-        method: "GET"
-      };
-      var response = await fetch(
-        "https://official-joke-api.appspot.com/random_ten",
-        config
-      );
-      var data = await response.json();
-
-      return data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  getJokesApi()
-    .then(function(data) {
-      random10Jokes = data;
-    })
-    .catch(function(e) {
-      console.error("We had a problem reaching the API!");
-      console.log(e);
-    });
-}
-
-function fetch1FromApi() {
-  async function getJokesApi() {
+function fetchJokeFromApi() {
+  async function getJokeApi() {
     try {
       config = {
         method: "GET"
@@ -47,7 +18,7 @@ function fetch1FromApi() {
     }
   }
 
-  getJokesApi()
+  getJokeApi()
     .then(function(data) {
       randomJoke = data;
     })
@@ -56,5 +27,4 @@ function fetch1FromApi() {
       console.log(e);
     });
 }
-fetch10FromApi();
-fetch1FromApi();
+fetchJokeFromApi();
